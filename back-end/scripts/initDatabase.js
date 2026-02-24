81d2dea9f36def3b5de01c69bb8ee76db95e43f2
Rishan Nadeera <n.rishee@gmail.com>
2026-02-25 00:59:38 +0530

    add changes to the init data base

--- back-end/scripts/initDatabase.js
+++ back-end/scripts/initDatabase.js
@@ -150,6 +150,8 @@ const createTables = async () => {
         website VARCHAR(255),
         companySize ENUM('1-5', '6-10', '11-20', '21-50', '51-200', '201-500', '500+') DEFAULT NULL,
         logo TEXT,
+        currencyId VARCHAR(10) DEFAULT NULL,
+        selectedEntities JSON DEFAULT NULL,
         isActive BOOLEAN DEFAULT TRUE,
         ownerId VARCHAR(10),
         createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
